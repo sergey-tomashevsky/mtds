3410c8346a814c8927afc4022ab94b8d6e25990e
Sergey Tomashevsky <sergey.tomashevsky@toughbyte.com>
2024-04-13 18:49:16 +0600

    Extracted offer update logic from Scraper to Card.update_offers method

--- lib/models/card.rb
+++ lib/models/card.rb
@@ -6,4 +6,16 @@ require_relative "card_offer"
 
 class Card < Sequel::Model
   one_to_many :offers, class: :CardOffer
+
+  def update_offers(offers_params)
+    DB.transaction do
+      # TODO: do not delist previous offers that weren't changed/removed.
+      # Delist previous offers.
+      offers_dataset.current.update(delisted: true, delisted_at: Time.now)
+      # Import new offers.
+      offers_params.each do |params|
+        add_offer(params)
+      end
+    end
+  end
 end

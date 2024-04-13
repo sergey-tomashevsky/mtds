9fc7b8526a17dfe46e34a14cdbcdd20727666cc7
Sergey Tomashevsky <sergey.tomashevsky@toughbyte.com>
2024-04-13 18:52:39 +0600

    Removed most of the logic from Scraper class Now it only handles multithreading and overall scrape progress. Maybe rename it something else now?

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,71 +1,47 @@
 # frozen_string_literal: true
 
-require "addressable/uri"
 require "concurrent-ruby"
 require "faraday"
 
 require_relative "../db/database"
 require_relative "models/card"
-require_relative "models/card_offer"
-require_relative "utils/html_document_parser"
+require_relative "single_card_scraper"
 
 class Scraper
-  SEARCH_URL = "https://mtgtrade.net/store/single/"
   REQUEST_TIMEOUT = 5
   THREAD_COUNT = 10
 
   def initialize
     @connection = Faraday.new { _1.options.timeout = REQUEST_TIMEOUT }
+    @total_cards = 0
+    @completed_cards = 0
   end
 
   def run
     query = Card.where(purchased: false).order(:name)
-    completed_cards = 0
-    total_cards = query.count
+    @total_cards = query.count
 
-    pool = Concurrent::ThreadPoolExecutor.new(
+    thread_pool = Concurrent::Threadthread_poolExecutor.new(
       min_threads: THREAD_COUNT,
       max_threads: THREAD_COUNT,
       max_queue: 0
     )
     query.each do |card|
-      pool.post do
-        import_card_offers(card)
-        completed_cards += 1
-        puts "Done looking up prices for #{card.name}... (#{completed_cards}/#{total_cards})"
+      thread_pool.post do
+        SingleCardScraper.new(card, @connection).run
+        @completed_cards += 1
+        puts "Done looking up prices for #{card.name} [#{progress}]"
       end
     end
 
-    pool.shutdown
-    pool.wait_for_termination
+    thread_pool.shutdown
+    thread_pool.wait_for_termination
     puts "Finished!"
   end
 
   private
 
-  def import_card_offers(card)
-    begin
-      response = @connection.get("#{SEARCH_URL}#{URI.encode_www_form_component(card.name)}")
-    rescue Faraday::ConnectionFailed
-      puts "Request timeout"
-      return
-    end
-
-    if response.status != 200
-      puts "Error: #{response.status}"
-      return
-    end
-
-    card_offers_params = HtmlDocumentParser.new(response.body).extract_card_offers_params
-
-    DB.transaction do
-      # TODO: do not delist previous offers that weren't changed/removed.
-      # Delist previous offers.
-      card.offers_dataset.current.update(delisted: true, delisted_at: Time.now)
-      # Import new offers.
-      card_offers_params.each do |params|
-        card.add_offer(params)
-      end
-    end
+  def progress
+    "#{@completed_cards}/#{@total_cards}"
   end
 end

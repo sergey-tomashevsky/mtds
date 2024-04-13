166470a0b8b8a45342a9d1a17cf1eb599673b228
Sergey Tomashevsky <sergey.tomashevsky@toughbyte.com>
2024-04-13 18:49:51 +0600

    Extracted most of the logic from Scraper to SingleCardScraper class

---
+++ lib/single_card_scraper.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "addressable/uri"
+require_relative "utils/html_document_parser"
+
+class SingleCardScraper
+  SEARCH_URL = "https://mtgtrade.net/store/single/"
+
+  def initialize(card, connection)
+    @card = card
+    @connection = connection
+  end
+
+  def run
+    html_body = request_store_page
+    card_offers_params = HtmlDocumentParser.new(html_body).extract_card_offers_params
+    @card.update_offers(card_offers_params)
+  end
+
+  private
+
+  def request_store_page
+    begin
+      response = @connection.get("#{SEARCH_URL}#{URI.encode_www_form_component(@card.name)}")
+    rescue Faraday::ConnectionFailed
+      puts "Request timeout"
+      return nil
+    end
+
+    if response.status != 200
+      puts "Error: #{response.status}"
+      return nil
+    end
+
+    response.body
+  end
+end
